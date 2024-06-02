296a641e478f5d5813347ab60ec4d15b17c5e534
KashtahApp <Alothman.Me@gmail.com>
2024-06-02 13:50:14 +0400

    first commit

---
+++ with_mutext.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Counter struct {
+	Value int
+	Mu    sync.Mutex
+}
+
+func (c *Counter) Increment() {
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+	c.Value++
+}
+
+func main() {
+	counter := &Counter{}
+
+	// Number of goroutines
+	const numGoroutines = 100
+
+	// WaitGroup to wait for all goroutines to finish
+	var wg sync.WaitGroup
+	wg.Add(numGoroutines)
+
+	for i := 0; i < numGoroutines; i++ {
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 1000; j++ {
+				counter.Increment()
+			}
+		}()
+	}
+
+	// Wait for all goroutines to finish
+	wg.Wait()
+
+	fmt.Printf("Final counter value: %d\n", counter.Value)
+}
